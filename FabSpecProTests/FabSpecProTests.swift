1a6744c4c6164b263486f81d5799de1f295523d5
Salvatore Militello <sjmilitello@hotmail.com>
2026-02-15 20:19:42 -0500

    Initial Commit

---
+++ FabSpecProTests/FabSpecProTests.swift
@@ -0,0 +1,16 @@
+//
+//  FabSpecProTests.swift
+//  FabSpecProTests
+//
+//  Created by Salvatore Militello on 2/15/26.
+//
+
+import Testing
+
+struct FabSpecProTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ FabSpecProUITests/FabSpecProUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  FabSpecProUITestsLaunchTests.swift
+//  FabSpecProUITests
+//
+//  Created by Salvatore Militello on 2/15/26.
+//
+
+import XCTest
+
+final class FabSpecProUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
